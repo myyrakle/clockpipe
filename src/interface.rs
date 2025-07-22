269a070ee8c4da38c01db838d8510a5ce7f0a87a
myyrakle <sssang97@naver.com>
2025-07-23 02:01:20 +0900

    [#1] pipe 기반 구조 작성

---
+++ src/interface.rs
@@ -0,0 +1,32 @@
+use crate::errors::Errors;
+
+#[async_trait::async_trait]
+pub trait IExporter {
+    async fn peek(&self) -> Result<PeekResult, Errors>;
+    async fn advance(&self, key: &str) -> Result<(), Errors>;
+}
+
+#[derive(Debug, Clone)]
+pub enum RowKey {
+    StringKey(String),
+    IntegerKey(i64),
+}
+
+#[derive(Debug, Clone)]
+pub enum EventType {
+    Insert,
+    Update,
+    Delete,
+}
+
+#[derive(Debug, Clone)]
+pub struct Row {
+    pub event_type: EventType,
+    pub key: RowKey,
+}
+
+#[derive(Debug, Clone)]
+pub struct PeekResult {
+    pub rows: Vec<Row>,
+    pub advance_key: String,
+}
---
+++ src/postgres/mod.rs
@@ -0,0 +1,18 @@
+use crate::{
+    errors::Errors,
+    interface::{IExporter, PeekResult},
+};
+
+#[derive(Debug, Clone)]
+pub struct PostgresExporter {}
+
+#[async_trait::async_trait]
+impl IExporter for PostgresExporter {
+    async fn peek(&self) -> Result<PeekResult, Errors> {
+        unimplemented!("Postgres peek not implemented yet");
+    }
+
+    async fn advance(&self, _key: &str) -> Result<(), Errors> {
+        unimplemented!("Postgres advance not implemented yet");
+    }
+}
