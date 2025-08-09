dcf142292c6a3a31f3c72e57e80641ebffa33769
myyrakle <sssang97@naver.com>
2025-08-10 02:06:45 +0900

    [#6] clickhouse adapter 구현

---
+++ src/adapter/clickhouse.rs
@@ -0,0 +1,33 @@
+use crate::errors;
+
+#[derive(Clone)]
+pub struct ClickhouseConnection {
+    client: clickhouse::Client,
+}
+
+impl ClickhouseConnection {
+    pub fn new(config: &crate::config::ClickHouseConnectionConfig) -> Self {
+        let client = clickhouse::Client::default()
+            .with_url(format!("http://{}:{}", config.host, config.port))
+            .with_user(config.username.clone())
+            .with_password(config.password.clone())
+            .with_database(config.database.clone());
+
+        ClickhouseConnection { client }
+    }
+
+    pub async fn ping(&self) -> errors::Result<()> {
+        self.client
+            .query("SELECT 1")
+            .fetch_one::<i64>()
+            .await
+            .map_err(|e| {
+                crate::errors::Errors::DatabasePingError(format!(
+                    "Failed to ping ClickHouse: {}",
+                    e
+                ))
+            })?;
+
+        Ok(())
+    }
+}
