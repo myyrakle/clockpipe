532c6853bce2afaf08f803d08a17dcc4920f2f85
myyrakle <sssang97@naver.com>
2025-08-15 02:30:21 +0900

    [#49] generate_insert_query를 trait method로 이식

--- src/adapter/mapper.rs
+++ src/adapter/mapper.rs
@@ -1,4 +1,8 @@
-use crate::adapter::clickhouse::ClickhouseType;
+use crate::{
+    adapter::{clickhouse::ClickhouseType, postgres::PostgresCopyRow},
+    config::ClickHouseConfig,
+    pipes::PostgresPipeTableInfo,
+};
 
 pub trait IntoClickhouseColumn {
     fn into_clickhouse_type(&self) -> ClickhouseType;
@@ -6,6 +10,8 @@ pub trait IntoClickhouseColumn {
     fn get_comment(&self) -> &str;
     fn is_in_primary_key(&self) -> bool;
 }
+
+pub trait IntoClickhouseRow {}
 
 pub trait IntoClickhouse {
     fn generate_create_table_query(
@@ -51,5 +57,79 @@ pub trait IntoClickhouse {
         query.push(';');
 
         query
+    }
+
+    fn generate_insert_query(
+        &self,
+        clickhouse_config: &ClickHouseConfig,
+        source_table_info: &PostgresPipeTableInfo,
+        table_name: &str,
+        rows: &[PostgresCopyRow],
+    ) -> String {
+        if rows.is_empty() {
+            return String::new();
+        }
+
+        let mut insert_query = format!(
+            "INSERT INTO {}.{table_name} ",
+            clickhouse_config.connection.database
+        );
+
+        let mut columns = vec![];
+        let mut column_names = vec![];
+
+        for clickhouse_column in &source_table_info.clickhouse_columns {
+            let Some(postgres_column) = source_table_info
+                .postgres_columns
+                .iter()
+                .find(|col| col.column_name == clickhouse_column.column_name)
+            else {
+                continue;
+            };
+
+            columns.push((clickhouse_column.clone(), postgres_column.clone()));
+            column_names.push(clickhouse_column.column_name.clone());
+        }
+
+        insert_query.push_str(&format!("({}) ", column_names.join(", ")));
+        insert_query.push_str("VALUES");
+
+        let mut values = vec![];
+
+        for row in rows {
+            let mut value = vec![];
+
+            for (clickhouse_column, _) in columns.iter() {
+                let Some(postgres_column) = source_table_info
+                    .postgres_columns
+                    .iter()
+                    .find(|col| col.column_name == clickhouse_column.column_name)
+                else {
+                    log::warn!(
+                        "Postgres column {} not found in ClickHouse table {}. Skipping.",
+                        clickhouse_column.column_name,
+                        table_name
+                    );
+                    continue;
+                };
+
+                let postgres_raw_column_value =
+                    row.columns.get(postgres_column.column_index as usize - 1);
+
+                let column_value = match postgres_raw_column_value {
+                    Some(raw_value) => clickhouse_column.value(raw_value.to_owned()),
+                    _ => clickhouse_column.default_value(),
+                };
+
+                value.push(column_value);
+            }
+
+            let value = value.join(",");
+            values.push(format!("({value})"));
+        }
+
+        insert_query.push_str(values.join(", ").as_str());
+
+        insert_query
     }
 }
