0dc8a35aa55a912de2c9274b0073551d648e35e1
myyrakle <sssang97@naver.com>
2025-08-16 00:17:55 +0900

    [#63] to_owned 제거

--- src/adapter/mod.rs
+++ src/adapter/mod.rs
@@ -66,12 +66,12 @@ pub trait IntoClickhouse {
 
         query.push_str(&column_definitions.join(", \n"));
 
-        let primary_keys: Vec<String> = columns
+        let primary_keys = columns
             .iter()
             .filter(|col| col.is_in_primary_key())
-            .map(|col| col.get_column_name().to_owned())
-            .collect();
-        let primary_keys = primary_keys.join(", ");
+            .map(|col| col.get_column_name())
+            .collect::<Vec<_>>()
+            .join(", ");
 
         query.push(')');
         query.push_str(" ENGINE = ReplacingMergeTree()");
