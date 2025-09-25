c6f3a9cceee404d00b73bdd509ec1bfa6e112f56
myyrakle <sssang97@naver.com>
2025-09-26 02:52:10 +0900

    v0.5.2

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,1 +1,1 @@
-sudo docker buildx build --platform linux/amd64,linux/arm64 -t myyrakle/clockpipe:v0.5.1 --push  .
+sudo docker buildx build --platform linux/amd64,linux/arm64 -t myyrakle/clockpipe:v0.5.2 --push  .
